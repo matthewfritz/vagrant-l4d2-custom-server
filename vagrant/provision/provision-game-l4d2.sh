0dc41a33ffc0b84b62bcad0918675325a9762507
Matthew Fritz <mattf@burbankparanormal.com>
2023-08-20 15:03:42 -0700

    Updates some install locations to absolute paths and allows the L4D2 server to be installed automatically during provisioning

--- vagrant/provision/provision-game-l4d2.sh
+++ vagrant/provision/provision-game-l4d2.sh
@@ -5,6 +5,7 @@
 STEAMCMD_MOUNT="/mnt/data/steamcmd"
 STEAMCMD_MAP_START="c2m1_highway"
 STEAMCMD_PORT="27020"
+STEAMCMD_HOME="/home/vagrant"
 
 output_line() {
     echo "[L4D2] $1"
@@ -19,12 +20,17 @@ tar xf steamcmd_linux.tar.gz
 output_line "Finished installing steamcmd"
 
 output_line "Creating Left 4 Dead 2 server startup script..."
-echo "~/l4d2_server/srcds_run -console -game left4dead2 +port ${STEAMCMD_PORT} +maxplayers 8 +exec ${STEAMCMD_MOUNT}/l4d2_server.cfg +map ${STEAMCMD_MAP_START}" > ~/start_l4d2.sh
-chmod +x ~/start_l4d2.sh
+echo "${STEAMCMD_HOME}/l4d2_server/srcds_run -console -game left4dead2 +port ${STEAMCMD_PORT} +maxplayers 8 +exec ${STEAMCMD_MOUNT}/l4d2_server.cfg +map ${STEAMCMD_MAP_START}" > ${STEAMCMD_HOME}/start_l4d2.sh
+chmod +x ${STEAMCMD_HOME}/start_l4d2.sh
 output_line "Finished creating startup script"
 
+output_line "Creating Left 4 Dead 2 server update script..."
+echo "${STEAMCMD_HOME}/steamcmd.sh +runscript ${STEAMCMD_MOUNT}/update_l4d2.txt" > ${STEAMCMD_HOME}/update_l4d2.sh
+chmod +x ${STEAMCMD_HOME}/update_l4d2.sh
+output_line "Finished creating update script"
+
 output_line "Running steamcmd with the custom update script..."
-#steamcmd +runscript ${STEAMCMD_MOUNT}/update_l4d2.txt
+./steamcmd.sh +runscript ${STEAMCMD_MOUNT}/update_l4d2.txt
 output_line "Finished running steamcmd"
 
 output_line "Finished Left 4 Dead 2 provisioning" 
